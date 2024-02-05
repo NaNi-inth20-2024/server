597d672b0ab2260677f0ecb326422555c4088c00
Maksym Ivannikov <ivannikovmax04@gmail.com>
2024-02-05 22:21:30 +0200

    (main) Partial setup: added requirements.ini, local settings, initialized project, env example

---
+++ charityAuctionProject/settings_local.py
@@ -0,0 +1,15 @@
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
