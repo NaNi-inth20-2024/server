7dfb42a4b4d12a628ef2056f040b43431a51f523
Antonio <98595473+tonujet@users.noreply.github.com>
2024-02-06 23:19:04 +0200

    Database models fixes

--- auction/models.py
+++ auction/models.py
@@ -8,7 +8,7 @@ MAX_AUCTION_TITLE_LENGTH = 50
 class Auction(models.Model):
     title = models.CharField(max_length=MAX_AUCTION_TITLE_LENGTH)
     description = models.TextField()
-    initial_bid = models.IntegerField()
+    initial_price = models.IntegerField()
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     active = models.BooleanField(default=True)
 
@@ -16,11 +16,9 @@ class Auction(models.Model):
 class Bid(models.Model):
     price = models.IntegerField()
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    active = models.BooleanField(default=True)
     auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
 
 
 class AuctionPhoto(models.Model):
     auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
     photo = models.ImageField(upload_to='auction_photos/')
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
