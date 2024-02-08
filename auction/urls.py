3f95ec8e91b6c8c58674d86a7e08a7ec00fa1952
Maksym Ivannikov <ivannikovmax04@gmail.com>
2024-02-08 23:44:18 +0200

    feat: Deleted redundant user listing routings

--- auction/urls.py
+++ auction/urls.py
@@ -1,10 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from auction.views import AuctionViewSet, BidViewSet, UserViewSet
+from auction.views import AuctionViewSet, BidViewSet
 
 router = DefaultRouter()
 router.register(r"auctions", AuctionViewSet, basename="auction")
 router.register(r"bids", BidViewSet, basename="bid")
-router.register(r"users", UserViewSet, basename="user")
 
 urlpatterns = router.urls
