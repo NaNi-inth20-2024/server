0ceb4c844c53e90fc8adaf388d8d9e10bf96bd0d
Antonio <98595473+tonujet@users.noreply.github.com>
2024-02-09 20:37:43 +0200

    Add ws endpoints to main api

--- auction/routing.py
+++ auction/routing.py
@@ -1,10 +1,10 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from django.urls import re_path, path, include
+from channels.routing import URLRouter
+from django.urls import re_path
+
 from auction import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/auctions/(?P<auction_id>\d+)/bids/$', consumers.AuctionConsumer.as_asgi()),
+    re_path(r'auctions/(?P<auction_id>\d+)/bids/$', consumers.AuctionConsumer.as_asgi()),
 ]
 
 router = URLRouter(websocket_urlpatterns)
--- charityAuctionProject/asgi.py
+++ charityAuctionProject/asgi.py
@@ -13,7 +13,6 @@ from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter
 from . import routing
-from auction import routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "charityAuctionProject.settings")
 
