1f8a9f54b6181a5f1e640d9be79abcb3d45a1694
Antonio <98595473+tonujet@users.noreply.github.com>
2024-02-11 01:06:31 +0200

    Add side-to-side communication to test ws api

--- charityAuctionProject/wss.py
+++ charityAuctionProject/wss.py
@@ -1,13 +1,16 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
+import json
 
 
 class MyConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         print("Hello")
         await self.accept()
+        await self.send(text_data=json.dumps("Helllo"))
 
     async def disconnect(self, close_code):
         pass
 
-    async def receive(selfl, text_data):
+    async def receive(self, text_data):
+        await self.send(text_data=json.dumps(f"Received: {text_data}"))
         pass
